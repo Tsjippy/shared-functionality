fcce51ded62f013267ce069fb2adfc049bad66cc
Tsjippy <ewaldha@hotmail.com>
2026-06-09 11:53:58 +0200

    skip:

---
+++ tsjippy-shared-functionality.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Plugin Name:          shared-functionality-loader
+ * Description:          Shared functionality for a bundle of 34 plugins
+ * Version:              10.6.7.3
+ * Author:               Ewald Harmsen
+ * Author URI:            https://harmseninnigeria.nl
+ * Requires at least:    6.3
+ * Requires PHP:         8.3
+ * Tested up to:         7.0
+ * Plugin URI:            https://github.com/Tsjippy/shared-functionality/
+ * Tested:                7.0
+ * TextDomain:            tsjippy
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * @author Ewald Harmsen
+ */
+
+namespace TSJIPPY;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+require_once(WP_PLUGIN_DIR . '/tsjippy-shared-functionality/loader.php');
