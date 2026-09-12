0064fd654ae113becce9a93b1baa0dc2c48e316c
Tsjippy <ewaldha@hotmail.com>
2026-09-12 07:25:28 +0200

    changed: removed 'tested up to'

--- tsjippy-shared-functionality.php
+++ tsjippy-shared-functionality.php
@@ -8,10 +8,9 @@
  * Author URI:            https://harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         7.1
- * Plugin URI:            https://github.com/Tsjippy/shared-functionality/
- * Tested:                7.1
- * TextDomain:            tsjippy
+ * Plugin URI:           https://github.com/Tsjippy/shared-functionality/
+ * Tested:               7.1
+ * TextDomain:           tsjippy
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
